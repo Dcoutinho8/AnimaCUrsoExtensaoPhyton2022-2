3913d5f55949dce22cda97d7d21fffbf6f432710
Dcoutinho8 <danilocoutti08@gmail.com>
2022-10-26 20:13:18 -0300

    Ensinado comando de entrada "input"

---
+++ aula2.py
@@ -0,0 +1,14 @@
+#Aula 2 
+#Comando input(): JOptionPane do Python
+# Qnd n se declara sempre vai ser Str 
+nome = input("Digite seu nome: ") 
+#X = int/Double (input("xxxxxxxxxxxxx"))
+idade = int(input("Qual a sua idade: "))
+
+#comando de saída.. exibir na tela 
+print(f"Boa noite {nome} você já tem {idade} anos")
+
+dobro = idade * 2 
+
+print("O drobro da sua idade é", dobro)
+
