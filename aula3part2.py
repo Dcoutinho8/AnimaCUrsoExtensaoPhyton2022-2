176b847afd929f44a71ef389f383fa9acd1d51ac
Dcoutinho8 <danilocoutti08@gmail.com>
2022-11-09 22:00:27 -0300

    Funções

--- aula3part2.py
+++ aula3part2.py
@@ -21,3 +21,20 @@ def calcular_imposto(preco_produto):
 preco = 299 
 imposto = calcular_imposto(preco)
 print(imposto)
+
+#agora calcular imposto a alíquota agora é 7% 
+def calcular_imposto(preco_produto): 
+    imposto = preco_produto * 0.5
+    return imposto
+
+#vc apenas troca o valor, caso n usasse a função teria q trocar um por um q vc utilizou essa conta de impsoto 
+
+preco = 299 
+imposto = calcular_imposto(preco)
+print(imposto)
+#Esse utiliza os 7% 
+
+valores = [1.99,24.50,78.27,1515.5]
+#Se eu quiser calcular o imposto destes quatros valores exemplo: "o imposto de... é ..." (1o. preço, 2o. imposto)
+for valor in valores:
+    print(f"o Imposto de {valores} é {calcular_imposto(valores)}")
