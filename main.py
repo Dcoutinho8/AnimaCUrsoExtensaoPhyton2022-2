e289c43999ac2bc54dc9a8d47fae77f239c237b6
Danilo Coutinho <115954418+Dcoutinho8@users.noreply.github.com>
2022-11-16 23:31:20 +0000

    Banco de Dados

--- main.py
+++
@@ -1,6 +0,0 @@
-#primeira programação de Python meu 
-name = 'Danilo'
-#se estiver entre "" é string 
-age = 18 
-#se estiver só numeros pode ser um "int" ou "double""
-print ("Olá, meu nome é",name,"e tenho",age,"anos") 
